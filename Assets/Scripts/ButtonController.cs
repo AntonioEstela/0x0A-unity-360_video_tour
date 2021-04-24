f8224ef2c530bff7440ddd09a3ff766fb279cb07
Antonio Estela <antonioestela73@gmail.com>
2021-04-23 19:26:39 -0500

    Initial commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public Text ButtonText;
+    public Animator animator;
+    public Image Image;
+
+    public void NextRoom()
+    {
+        StartCoroutine(Fade());
+        SceneManager.LoadScene(ButtonText.text + "Scene");
+    }
+
+    IEnumerator Fade()
+    {
+        animator.SetBool("IsFading", true);
+        yield return new WaitUntil(() => Image.color.a == 1);
+    }
+}
---
+++ Assets/Scripts/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageController : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void ActivateMessage()
+    {
+        Panel.SetActive(!Panel.activeInHierarchy);
+    }
+}
